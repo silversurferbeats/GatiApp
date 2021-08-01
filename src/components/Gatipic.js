492e93320b4a3957cec5a2ceea2be0aec64fb6c0
silversurferbeats <gabrielsuarez89@gmail.com>
2021-08-01 01:37:02 -0300

    arego estados hook

--- src/components/Gatipic.js
+++ src/components/Gatipic.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import GetGati from '../service/index';
-//import { useState, useEffect } from 'react';
 
 
-class Gatipic extends Component {
-  render() {
+const Gatipic = () => {
 
-    
+  const [GatiUrl, setGatiUrl] = useState();
 
-    return (
-      <div>
-        <img
-          id={1}
-          className="imagen"
-          src="https://cataas.com/cat/5ec587d0fc6b05001805d6bb"
-        />
-        <button
-          className="boton"
-        >
-        Next
-        </button>
-      </div>
-    );  
+  
+  const handleGati = async () => {
+    //aca va el swich del useState
+    let newUrl = await GetGati;
+    setGatiUrl(newUrl);
   }
+
+  useEffect(() => {
+    handleGati();
+  },[]);
+
+  console.log(handleGati());
+   
+
+  return(
+    <div>
+      <img
+        id={1}
+        className="imagen"
+        src={GatiUrl}
+      />
+      <button
+        className="boton"
+        onClick={() => handleGati()}
+      >
+      Next
+      </button>
+    </div>
+  )
 }
- export default Gatipic;
+export default Gatipic;
--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,9 @@
 
 
-export  const GetGati = async () => {
+const GetGati = async () => {
     const data = await fetch('https://cataas.com/cat?json=true');
     const value = await data.json();
-    return console.log(value);
+    return `https://cataas.com${value.url}`;
 }
 
 export default GetGati();
